2d26910fa09cf83a67548789db42d999a9ecd7d2
jordankiggundu <jordankiggundu@gmail.com>
2024-06-12 15:26:34 +0300

    plugin registrar push

--- android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
+++ android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
@@ -26,6 +26,11 @@ public final class GeneratedPluginRegistrant {
       Log.e(TAG, "Error registering plugin firebase_core, io.flutter.plugins.firebase.core.FlutterFirebaseCorePlugin", e);
     }
     try {
+      flutterEngine.getPlugins().add(new io.flutter.plugins.firebase.firebase_ml_model_downloader.FirebaseModelDownloaderPlugin());
+    } catch (Exception e) {
+      Log.e(TAG, "Error registering plugin firebase_ml_model_downloader, io.flutter.plugins.firebase.firebase_ml_model_downloader.FirebaseModelDownloaderPlugin", e);
+    }
+    try {
       flutterEngine.getPlugins().add(new io.flutter.plugins.flutter_plugin_android_lifecycle.FlutterAndroidLifecyclePlugin());
     } catch (Exception e) {
       Log.e(TAG, "Error registering plugin flutter_plugin_android_lifecycle, io.flutter.plugins.flutter_plugin_android_lifecycle.FlutterAndroidLifecyclePlugin", e);
